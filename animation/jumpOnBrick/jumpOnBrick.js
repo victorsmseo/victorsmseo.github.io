752edc5e88b130ea9cc7cec220000acf472c463f
victorsmseo <victorsmseo@gmail.com>
2018-12-04 14:17:22 -0800

    Refactor version 1

--- animation/jumpOnBrick/jumpOnBrick.js
+++ animation/jumpOnBrick/jumpOnBrick.js
@@ -1,19 +1,19 @@
 const LOOP_TIME = 20;
 const MARIO_WIDTH = 10;
-const JUMP_POSITION = 35;
-const BRICK_POSITION = 80;
-const FALL_POSITION = 192;
-const LAND_POSITION = 237;
 const SPEED = 1;
 const ACCELERATION = 0.05;
+
+var WINDOW_WIDTH;
+var WINDOW_HEIGHT;
+var JUMP_POSITION;
+var BRICK_POSITION;
+var FALL_POSITION;
+var LAND_POSITION;
+var POSITION_Y;
 
 var canvas;
 var ctx;
 var requestId;
-
-var WINDOW_WIDTH;
-var WINDOW_HEIGHT;
-var POSITION_Y;
 
 var isJumping = false;
 var positionX = -MARIO_WIDTH;
@@ -27,22 +27,26 @@ window.onload = init;
 function init() {
     canvas = document.getElementById("myCanvas");
     ctx = canvas.getContext("2d");
-    ctx.canvas.width  = 0.85*window.innerWidth;
-    ctx.canvas.height = 0.7*window.innerHeight;
+    ctx.canvas.width = 0.85 * window.innerWidth;
+    ctx.canvas.height = 0.7 * window.innerHeight;
 
     WINDOW_HEIGHT = canvas.height / unit;
     WINDOW_WIDTH = canvas.width / unit;
-    POSITION_Y = WINDOW_HEIGHT-32;
+    BRICK_POSITION = WINDOW_WIDTH/3;
+    FALL_POSITION = 2*WINDOW_WIDTH/3;
+    JUMP_POSITION = BRICK_POSITION-45;
+    LAND_POSITION = FALL_POSITION+45;
+    POSITION_Y = WINDOW_HEIGHT - 32;
 
-	startAnimation();
+    startAnimation();
 }
 
 function startAnimation() {
-	requestId = requestAnimationFrame(animationLoop);
+    requestId = requestAnimationFrame(animationLoop);
 }
 
 function animationLoop(timeStamp) {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     drawBackground();
     animateMario();
@@ -51,16 +55,16 @@ function animationLoop(timeStamp) {
     changePositionX();
     changePositionY();
 
-	requestId = requestAnimationFrame(animationLoop);
+    requestId = requestAnimationFrame(animationLoop);
 }
 
 function animateMario() {
     if (isJumping) {
         drawMario3(positionX, positionY);
     } else {
-        if (0 <= time && time < LOOP_TIME/2) {
+        if (0 <= time && time < LOOP_TIME / 2) {
             drawMario2(positionX, positionY);
-        } else if (LOOP_TIME/2 <= time && time <= LOOP_TIME) {
+        } else if (LOOP_TIME / 2 <= time && time <= LOOP_TIME) {
             drawMario3(positionX, positionY);
         }
     }
@@ -77,11 +81,11 @@ function changeIsJumping() {
 }
 
 function changePositionX() {
-	if (positionX <= WINDOW_WIDTH) {
-		positionX += SPEED;
-	} else {
-		positionX = -MARIO_WIDTH;
-	}
+    if (positionX <= WINDOW_WIDTH) {
+        positionX += SPEED;
+    } else {
+        positionX = -MARIO_WIDTH;
+    }
 }
 
 function changePositionY() {
@@ -92,9 +96,10 @@ function changePositionY() {
         positionY += jumpSpeed;
         jumpSpeed += ACCELERATION;
     } else if ((BRICK_POSITION <= positionX) && (positionX < FALL_POSITION)) {
-        positionY = WINDOW_HEIGHT-96;
+        positionY = WINDOW_HEIGHT - 96;
     } else {
         positionY = POSITION_Y;
+        jumpSpeed = 2.55;
     }
 }
 
@@ -107,8 +112,8 @@ function changeTime() {
 }
 
 function drawBackground() {
-	ctx.fillStyle = "#5B8BFF"; // Sky Blue
-	ctx.fillRect(0,0,canvas.width,canvas.height);
-    drawGroundBlocks(0,WINDOW_HEIGHT-16,WINDOW_WIDTH/16,1);
-    drawBrickBlocks(80,WINDOW_HEIGHT-80,7,1);
+    ctx.fillStyle = "#5B8BFF"; // Sky Blue
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    drawGroundBlocks(0, WINDOW_HEIGHT - 16, WINDOW_WIDTH / 16, 1);
+    drawBrickBlocks(BRICK_POSITION, WINDOW_HEIGHT - 80, (WINDOW_WIDTH / 48) - 1, 1);
 }
